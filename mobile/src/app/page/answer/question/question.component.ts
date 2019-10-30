14bfd4c4cefb0bfb9b7fa9295830c699c7c71bb7
ECarrignon <carrignon.emilien@hotmail.fr>
2019-10-30 12:49:28 +0100

    service validation init

--- mobile/src/app/page/answer/question/question.component.ts
+++ mobile/src/app/page/answer/question/question.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { NavController } from '@ionic/angular';
 import { Question } from 'src/app/model/referentiels/question';
 import { AlertController } from '@ionic/angular';
-import { Alert } from 'selenium-webdriver';
+import { ValidationService } from 'src/app/service/ValidationService';
 
 @Component({
   selector: 'app-question',
@@ -14,42 +13,18 @@ export class QuestionComponent implements OnInit {
 
   /** Question à trouver */
   question: Question;
-  /** Message d'erreur */
-  errorMessage: string;
   /** reponse */
   reponse: string;
 
   constructor(
     private route: ActivatedRoute, 
-    private navController: NavController,
-    public alertController: AlertController) {}
+    private validationService: ValidationService) {}
 
   ngOnInit() {
     this.question = JSON.parse(this.route.snapshot.paramMap.get('question'));
   }
 
   validerSaisie(){
-    let reponseCorrecte: Boolean = this.reponse == '1';
-    if(reponseCorrecte){
-      this.errorMessage=null;
-      this.navController.navigateRoot(['/home']);
-    } else{
-      this.errorMessage="Réponse incorrecte"
-    }
-    this.presentAlert();
+    this.validationService.validerQuestion(this.reponse, null);
   }
-
-
-  async presentAlert() {
-    const alert = await this.alertController.create({
-      message: 'This is an alert message.'
-    });
-
-    await alert.present();
-    setTimeout(() => {
-      alert.dismiss();
-    }, 800);
-
-  }
-  
 }
