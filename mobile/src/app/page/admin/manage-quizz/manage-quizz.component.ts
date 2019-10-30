749dd62429b2c47d2a52c73d31b704e20b3af6bf
Alexis BOUVARD <alexis.bouvard@soprasteria.com>
2019-10-30 14:16:50 +0100

    Init management quizz

--- mobile/src/app/page/admin/manage-quizz/manage-quizz.component.ts
+++ mobile/src/app/page/admin/manage-quizz/manage-quizz.component.ts
@@ -7,8 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageQuizzComponent implements OnInit {
 
+  listQuizz=[{
+    name: 'Quizz jour 1',
+    nbIndices: 11,
+    gagnant: null,
+    status: 'Initialisé',
+    dateOfStart: null,
+    dateOfEnd: null,
+  }, {
+    name: 'Quizz jour 2',
+    nbIndices: 12,
+    gagnant: null,
+    status: 'Initialisé',
+    dateOfStart: null,
+    dateOfEnd: null,
+  }]
+
   constructor() { }
 
   ngOnInit() {}
+
+  startQuizz(quizzName) {
+    for(let quizz of this.listQuizz) {
+      if(quizz.name == quizzName) {
+        if(quizz.status=='Initialisé') {
+          quizz.status='Démarré';
+          quizz.dateOfStart = new Date();
+        }
+      }
+    }
+  }
+
+  stopQuizz(quizzName) {
+    for(let quizz of this.listQuizz) {
+      if(quizz.name == quizzName) {
+        if(quizz.status=='Démarré') {
+          quizz.status='Arrêté';
+          quizz.dateOfEnd = new Date();
+        }
+      }
+    }
+  }
 
 }
